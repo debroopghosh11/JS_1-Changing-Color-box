fb1ce7f07017df04555c2bdc5010c5c5ada350d7
Debroop Ghosh <debroopghosh2012@gmail.com>
2023-12-09 23:50:49 +0530

    made some changes

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 var rect = document.querySelector("#center"); 
 
-rect.addEventListener("mousemove",function(){
-    
-})
+rect.addEventListener("mousemove",function(details){
+
+    var rectangleLocation = rect.getBoundingClientRect();
+    var insiderectval = details.clientX - rectangleLocation.left;
+
+    if (insiderectval < rectangleLocation.width / 2){
+        console.log("left");
+
+    }
+    else {
+        console.log("right");
+    }
+});
